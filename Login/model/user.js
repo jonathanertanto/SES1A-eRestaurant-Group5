0315b470e634bd192629b9c51b25f4d0b8964337
jonathanertanto <jonathanertanto@gmail.com>
2021-09-02 14:18:35 +0700

    Unfinished SignUp update

--- Login/model/user.js
+++ Login/model/user.js
@@ -2,12 +2,12 @@ const mongoose = require ("mongoose")
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true},
-  password: { type: String, required: true}
-  // email: { type: String, required: true, unique: true},
-  // first_name: { type: String, required: true},
-  // last_name: { type: String},
-  // date_of_birth: {type: Date},
-  // contact_number: {type: String}
+  email: { type: String, required: true, unique: true},
+  password: { type: String, required: true},
+  first_name: { type: String, required: true},
+  last_name: { type: String},
+  date_of_birth: {type: Date},
+  contact_number: {type: String}
 
 }, {collection: 'users'})
 
